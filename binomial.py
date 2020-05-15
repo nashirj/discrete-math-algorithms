0597279e389c0401a361c90861a1be70a954d480
Nashir Janmohamed <nashirbass@gmail.com>
2020-05-15 13:02:36 -0700

    implemented computing combinations with repetition

--- binomial.py
+++ binomial.py
@@ -1,10 +1,14 @@
 import math
+# from typing import
 
 def n_choose_k(n,k):
     return math.factorial(n)//(math.factorial(n-k) * math.factorial(k))
 
 def n_pick_k(n,k):
     return math.factorial(n)//math.factorial(n-k)
+
+def n_choose_k_repetition_allowed(n,k):
+    return n_choose_k(n+k-1,k)
 
 def n_pick_k_repetition_allowed(n,k):
     return n**k
