47682f93cf6fe13c0437aea802d842602fe2153a
Nashir Janmohamed <nashirbass@gmail.com>
2020-05-27 20:41:41 -0700

    Create benchmark file for comparing python and c implementations

---
+++ benchmark.py
@@ -0,0 +1,20 @@
+import sets
+import csets
+import time
+
+def compare_run_times(functions, args):
+    for function in functions:
+        t0 = time.time()
+        res = function(args)
+        print(res)
+        t1 = time.time()
+        print(f"Function {function.__name__} took {t1-t0} seconds to run")
+
+def main():
+    arg = [i+1 for i in range(4)]
+    # functions = [sets.generate_power_set, sets.generate_cartesian_product]
+    compare_run_times([sets.generate_power_set, csets.generate_pset], arg)
+
+if __name__ == '__main__':
+    main()
+    
