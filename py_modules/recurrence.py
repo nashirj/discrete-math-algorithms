6ebe5d467fd571a0f3d9a6bde68614ce1bc63592
Ariel Young <arielyoung103@gmail.com>
2020-06-03 13:29:02 -0700

    Removed debug prints

--- py_modules/recurrence.py
+++ py_modules/recurrence.py
@@ -6,14 +6,10 @@ def solve_lin_recurrence_relation(coefficients: list, base_cases: list):
 	coefficients = np.multiply(coefficients, -1)
 	roots = np.roots(coefficients)
 
-	print(f"Coefficients: {coefficients}")
-	print(f"Roots: {roots}")
-
 	matrix = []
 	for i in range(len(base_cases)):
 		matrix.append(np.power(roots, i))
 
-	print(matrix)
 	inv = np.linalg.inv(np.array(matrix))
 	base = np.array(base_cases)
 
@@ -23,4 +19,3 @@ def solve_lin_recurrence_relation(coefficients: list, base_cases: list):
 		ret.append((res[i], roots[i]))
 
 	return ret
-	
